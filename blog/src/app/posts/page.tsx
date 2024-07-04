56d2b1ae04e22357c2f02b7cd1e124d4dd142523
Mayo <mayo@moon.horse>
2024-07-03 22:50:57 -0700

    Adds dynamic posts page.

--- blog/src/app/posts/page.tsx
+++ blog/src/app/posts/page.tsx
@@ -1,22 +1,59 @@
 import fs from "fs/promises";
+import { compileMDX } from "next-mdx-remote/rsc";
 import path from "path";
+import { parse } from "yaml";
+import { MakeClass } from "../../utilities/MakeClass";
+import { THEME } from "../../theme";
+import { CLASS } from "../../classes";
+import Link from "next/link";
 
 export default async function Page() {
-  let result = await fs.readdir(path.join(process.cwd(), "src/app/posts"));
+  let result = await fs.readdir(path.join(process.cwd(), "src/posts"));
 
-  // filter to directories only
-  result = result.filter((f) => !f.includes("."));
+  // filter to mdx only
+  result = result.filter((f) => f.includes(".mdx"));
 
-  /* BEGIN: ed8c6549bwf9
   result = await Promise.all(
     result.map((f) => {
-      return fs.readFile(
-        path.join(process.cwd(), "src/app/posts", f, "page.mdx"),
-        "utf-8"
-      );
+      return fs.readFile(path.join(process.cwd(), "src/posts", f), "utf-8");
     })
   );
-  END: ed8c6549bwf9 */
 
-  return <div>{result.join(" ")}</div>;
+  const posts = result.map((p) => {
+    const match = p.match(/---[\s\S]*---/g);
+
+    if (!match) throw new Error("No frontmatter found");
+
+    const frontmatter = parse(match[0].replace(/---/g, ""));
+
+    return {
+      title: frontmatter.title,
+      date: frontmatter.date,
+      slug: frontmatter.slug ?? "",
+    };
+  });
+
+  return (
+    <div className={MakeClass(THEME.colors.bgPrimary, CLASS.Full)}>
+      <div className="flex flex-col gap-4 p-4">
+        {posts.map((p) => {
+          return (
+            <Link
+              className={MakeClass(
+                THEME.colors.bgSecondary,
+                "p-4",
+                "rounded",
+                "shadow-lg",
+                THEME.colors.textPrimary
+              )}
+              href={`/posts/${p.slug}`}
+            >
+              <h1 className="text-2xl">{p.title}</h1>
+              <p>{p.date}</p>
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
