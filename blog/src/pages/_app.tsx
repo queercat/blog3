1ec8553fad9e444d54fd3822fd17978872b3b93a
Mayo <mayo@moon.horse>
2024-07-03 22:50:58 -0700

    Begins ignoring fonts. Also this looks good.

--- blog/src/pages/_app.tsx
+++ blog/src/pages/_app.tsx
@@ -2,10 +2,13 @@ import "./globals.css";
 import { MakeClass } from "../utilities/MakeClass";
 import Link from "next/link";
 import { AppProps } from "next/app";
+import { THEME } from "../theme";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="flex flex-col h-full">
+    <div
+      className={MakeClass("flex flex-col h-full", THEME.colors.textPrimary)}
+    >
       <div
         className={MakeClass(
           "flex",
@@ -19,12 +22,27 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <Link href={"/"}>home</Link>
         <p> - </p>
         <Link href={"/posts"}>posts</Link>
-        <p> - </p>
+        {/* <p> - </p>
         <Link href={"/tools"}>tools</Link>
         <p> - </p>
-        <Link href={"/scripts"}>scripts</Link>
+        <Link href={"/scripts"}>scripts</Link> */}
       </div>
       {Component && <Component {...pageProps} />}
+      <div
+        className={MakeClass(
+          "flex",
+          "justify-center",
+          "p-4",
+          "gap-4",
+          THEME.colors.bgSecondary,
+          "text-lg",
+          "text-neutral-400"
+        )}
+      >
+        <p>all views are my own</p>
+        <p>-</p>
+        <Link href={"https://github.com/queercat"}>github</Link>
+      </div>
     </div>
   );
 }
